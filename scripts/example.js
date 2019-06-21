eed9a10137bcd834af5689b211c2ff0f9a81fdb7
sapphi-red <green@sapphi.red>
2019-06-21 13:31:03 +0900

    Fix: example.js

--- scripts/example.js
+++ scripts/example.js
@@ -1,6 +1,6 @@
 module.exports = robot => {
   // "@botName hoge"を受け取ったら"@senderName fuga"を送り返す
-  robot.respond(/^hoge$/i, res => {
+  robot.respond(/hoge$/i, res => {
     res.reply("fuga");
   });
 };
